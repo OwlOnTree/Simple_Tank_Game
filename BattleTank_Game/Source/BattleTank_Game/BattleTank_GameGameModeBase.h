2ba9daccb7718cf537e64d98ace15314583c84ac
Bogdan <bogdan7937@gmail.com>
2018-11-14 20:00:43 +0300

    Create a project with UE 4.18.3 and add new map(BattleMap)

---
+++ BattleTank_Game/Source/BattleTank_Game/BattleTank_GameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_GAME_API ABattleTank_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
